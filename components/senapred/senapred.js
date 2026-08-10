031ce19a9c04aa37b101765ce819f20b8e9fdead
ecastilloluengo <ecastilloluengo@gmail.com>
2026-08-10 16:35:24 -0400

    Muestra hora SENAPRED en formato 24 horas

--- components/senapred/senapred.js
+++ components/senapred/senapred.js
@@ -68,7 +68,8 @@
                 month: "2-digit",
                 year: "numeric",
                 hour: "2-digit",
-                minute: "2-digit"
+                minute: "2-digit",
+                hour12: false
             }
         ).format(fecha);
     }
