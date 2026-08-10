63f85b5d6742f22349dfacb6b10651b20fba5a4f
ecastilloluengo <ecastilloluengo@gmail.com>
2026-08-10 16:54:16 -0400

    Ajusta formato horario y actualiza cache SENAPRED

--- components/senapred/senapred.js
+++ components/senapred/senapred.js
@@ -44,7 +44,8 @@
                 day: "2-digit",
                 month: "short",
                 hour: "2-digit",
-                minute: "2-digit"
+                minute: "2-digit",
+            hour12: false
             }
         ).format(fecha);
     }
